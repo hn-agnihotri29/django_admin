fd5897b7ce0b00d41cf38f6690a5cb50782e48d8
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-06-01 21:50:27 +0530

    use jwt for authentication and added the logout and login features

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import users, register
+from .views import users, register, login, AuthenticatedUser, logout
 
 
 urlpatterns = [
     path('users', users),
-    path('register', register)
+    path('register', register),
+    path('login', login),
+    path('user', AuthenticatedUser.as_view()),
+    path('logout', logout),
 ]
