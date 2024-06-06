366df6519aca005083f378d86525f98996c18973
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-06-06 21:07:00 +0530

    add chart api to get data for chart in frontend application

--- orders/views.py
+++ orders/views.py
@@ -50,3 +50,26 @@ class ExportAPIView(APIView):
                 writer.writerow(['', '', '', item.product_title, item.price, item.quantity])
 
         return response
+
+class ChartAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, _):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT DATE_FORMAT(o.created_at, '%Y-%m-%d') as date, sum(i.quantity * i.price) as sum
+            FROM orders_order as o
+            JOIN orders_orderitem as i ON o.id = i.order_id
+            GROUP BY date
+            """)
+            row = cursor.fetchall()
+
+        data = [{
+            'date': result[0],
+            'sum': result[1]
+        } for result in row]
+
+        return Response({
+            'data': data
+        })
